c939c19442425af91469eb880b5e24af9328f02f
webmiss54 <webmiss54@gmail.com>
2017-07-19 22:39:08 +0200

    mon test waza

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ var wazaApp = angular.module('wazaApp', ['ngRoute']);
 
 wazaApp.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
-	when('/',{
-		templateUrl : 'views/demo.html',
-	})
-	.otherwise
-	redirecTo : '/'
+	.when('/users',{
+        templateUrl : 'partials/users.html',
+        controller: 'AdminUsersCtrl'
+    })
+	.otherwise({
+	redirectTo : '/'
 });
 }]);
